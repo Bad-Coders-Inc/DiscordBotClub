eb95fef054bf6fa5774a931fabb6164ef14226ee
sarthyparty <sarthavader@gmail.com>
2020-08-19 12:36:26 -0500

    setup bot:

--- bot.py
+++ bot.py
@@ -1,31 +1,30 @@
-import discord
 
-from bot_token import TOKEN
+from dtoken import TOKEN
 
 from discord.ext import commands
-
-import chatbot
 
 bot = commands.Bot(command_prefix='', description="Codeucate's Customer Service Bot")
 @bot.event
 async def on_ready():
 
-    print('Logged in as')
+	print('Logged in as')
 
-    print(bot.user.name)
+	print(bot.user.name)
 
-    print(bot.user.id)
+	print(bot.user.id)
 
-    print('------')
+	print('------')
 
 
 
 @bot.event
 async def on_message(message):
-    
-
+	await bot.process_commands(message)
 async def on_member_join(member):
-    await member.send("Hello! I am a bot here to help you, and I'll be there to answer any questions about our club that you might have!")
+	await member.send("Hello! I am a bot here to help you, and I'll be there to answer any questions about our club that you might have!")
+@bot.command()
+async def hello(ctx):
+	await ctx.send("Hi!!")
 
 
 bot.run(TOKEN)

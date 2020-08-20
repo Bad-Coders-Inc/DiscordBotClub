df0c215f66afa011d8dfd3cf580f366b9e652ae8
sarthyparty <sarthavader@gmail.com>
2020-08-20 13:29:25 -0500

    updated bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,13 @@
+import discord
 
 from dtoken import TOKEN
 
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='', description="Codeucate's Customer Service Bot")
+roles_assigned = True
+guild = None
+the_user = None
+bot = commands.Bot(command_prefix='+', description="Codeucate's Customer Service Bot")
 @bot.event
 async def on_ready():
 
@@ -15,23 +19,21 @@ async def on_ready():
 
 	print('------')
 
-rolesAssigned = False
-guild = None
-
 @bot.event
 async def on_message(message):
-
-	if not rolesAssigned:
-		selected_roles = message.content.split(",")
+	global roles_assigned
+	global the_user
+	global guild
+	if not roles_assigned and the_user == message.author:
+		selected_roles = message.content.replace(' ','').split(',')
 		for a_role in selected_roles:
-			role_name = a_role.lower()
+			role_name = a_role.title()
 			try:
-				user = message.author
 				role = discord.utils.get(guild.roles, name=role_name)
-				await client.add_roles(user, role)
+				await the_user.add_roles(role)
 			except:
 				await message.author.send(f"Could not find {a_role}")
-		rolesAssigned=True
+		roles_assigned=True
 	else:
 		await bot.process_commands(message)
 
@@ -43,22 +45,26 @@ async def on_message(message):
 
 @bot.event
 async def on_member_join(member):
+	global guild
 	guild = member.guild
+	global the_user
+	the_user  = member
 	await member.send("Hello! I am a bot here to help you, and I'll be there to answer any questions about our club that you might have!")
-	if not rolesAssigned:
-		prompt = '''
-		Which programming languages do you use?
-		-Python
-		-Java
-		-C
-		-C++
-		-C#
-		-Swift
-		-Other
-		-No experience
-		(Please seperate your choices by commas only):
-		'''
-		await member.send(prompt)
+	global roles_assigned
+	roles_assigned = False
+	prompt = '''
+	Which programming languages do you use?
+	-Python
+	-Java
+	-C
+	-C++
+	-C#
+	-Swift
+	-Other
+	-No experience
+	(Please seperate your choices by commas only):
+	'''
+	await member.send(prompt)
 
 
 @bot.command()

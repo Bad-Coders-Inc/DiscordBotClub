e6fed66e564c29cde708f9c7a111d412a8ea75f6
Sameen_R <64009883@ep-student.org>
2020-08-24 12:51:01 -0500

    tried adding new command

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ data = {}
 
 guild = None
 users = []
-bot = commands.Bot(command_prefix='>', description="Codeucate's Customer Service Bot")
+bot = commands.Bot(command_prefix='*', description="Welcome to Bad Coder's Inc Club!")
 @bot.event
 async def on_ready():
 
@@ -27,7 +27,6 @@ async def on_message(message):
 	global users
 	global guild
 	if message.author in users and "Direct Message" in str(message.channel):
-		print(message.channel)
 		for user in users:
 			if message.author == user:
 				the_user = user
@@ -41,14 +40,9 @@ async def on_message(message):
 			except:
 				await message.author.send(f"Could not find {a_role}")
 		users.remove(the_user)
-		print(users)
+		await message.channel.send("Please type in *updateuser **firstname-lastname email**")
 	else:
 		await bot.process_commands(message)
-
-
-
-
-
 
 
 @bot.event
@@ -60,29 +54,14 @@ async def on_member_join(member):
 	await member.send("Welcome to the bad coders inc club! To get started, please answer the following prompt.")
 	prompt = '''
 	What programming languages are you familiar with?
-	-Python
-	-Java
-	-C
-	-C++
-	-C#
-	-Swift
-	-Other
-	-No experience
+
 	(Please seperate your choices by commas only):
 	'''
 	await member.send(prompt)
 
 
 @bot.command()
-async def hello(ctx):
-	await ctx.send("Hi!!")
-
-@bot.command()
-async def hi(ctx):
-	await ctx.send("Hi!!")
-
-@bot.command()
-@commands.has_role("Admin")
+@commands.has_role("bad coder")
 async def add(ctx, member: discord.Member, project):
 	global data
 	with open('projects.json', 'r') as datafile:
@@ -91,6 +70,10 @@ async def add(ctx, member: discord.Member, project):
 			data['projects'][project]['members'].append(data['members'][str(member.id)]['name'])
 		except:
 			await ctx.send("Could not find that member.")
+
+@bot.command()
+async def hello(ctx):
+	await ctx.send("Hello World")
 
 
 @bot.command()
@@ -135,19 +118,25 @@ async def list(ctx, what, open_only='open'):
 		else:
 			await ctx.send("Please type in members or projects :)")
 
+
+
 @bot.command()
 async def create(ctx, name, link):
-	new={
-	name: {
-	"members" :[],
-	"link":link,
-	"status": "open"
-	}
-	}
+	# new={
+	# "members" :[],
+	# "link":link,
+	# "status": "open"
+	# }
 	with open('projects.json','r') as datafile:
 		data=json.load(datafile)
-		data["projects"].update(new)
-		datafile.close()
+		new={
+		"members" :[data['members'][str(ctx.author.id)]["name"]],
+		"link":link,
+		"status": "open"
+		}
+		data["projects"][name] = new
+	with open('projects.json', 'w') as datafile:
+		json.dump(data, fp = datafile, indent = 4)
 
 @bot.command()
 async def project(ctx, name):
@@ -160,7 +149,7 @@ async def updateuser(ctx, Name, email):
 	name = names[0] + ' ' + names[1]
 	with open('projects.json', 'r') as datafile:
 		data = json.load(datafile)
-		data['members'][ctx.author.id] = {"name": name, "status": "Member", "email": email}
+		data['members'][str(ctx.author.id)] = {"name": name, "status": "Member", "email": email}
 
 	with open('projects.json', 'w') as datafile:
 		json.dump(data, fp = datafile, indent = 4)

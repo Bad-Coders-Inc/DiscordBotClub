53638415a9ecc7bf6fc5da69ea25958e634c6b43
Sameen_R <64009883@ep-student.org>
2020-08-24 14:14:50 -0500

    made categories

--- bot.py
+++ bot.py
@@ -60,27 +60,8 @@ async def on_member_join(member):
 	await member.send(prompt)
 
 
-@bot.command()
-<<<<<<< HEAD
-async def hello(ctx):
-	await ctx.send("Hi!!")
 
-@bot.command()
-async def hi(ctx):
-	await ctx.send("Hi!!")
 
-@bot.command()
-=======
->>>>>>> testingCategories
-@commands.has_role("bad coder")
-async def add(ctx, member: discord.Member, project):
-	global data
-	with open('projects.json', 'r') as datafile:
-		try:
-			data = json.load(datafile)
-			data['projects'][project]['members'].append(data['members'][str(member.id)]['name'])
-		except:
-			await ctx.send("Could not find that member.")
 
 class Miscellaneous(commands.Cog):
 	def __init__(self, bot):
@@ -97,17 +78,63 @@ class Miscellaneous(commands.Cog):
 bot.add_cog(Miscellaneous(bot))
 
 
-@bot.command()
-async def list(ctx, what, open_only='open'):
-	with open('projects.json', 'r') as datafile:
-		data=json.load(datafile)
-		line=""
-		if (what=="projects"):
-			for project in data["projects"].keys():
-				value = data["projects"][project]
-				status = value['status']
-				if open_only=="open":
-					if status=='open':
+
+
+class Project_Leaders(commands.Cog):
+	def __init__(self, bot):
+		self.bot=bot
+
+	@commands.command()
+	@commands.has_role("bad coder")
+	async def create(self, ctx, name, link):
+		with open('projects.json','r') as datafile:
+			data=json.load(datafile)
+			new={
+			"members" :[data['members'][str(ctx.author.id)]["name"]],
+			"link":link,
+			"status": "open"
+			}
+			data["projects"][name] = new
+		with open('projects.json', 'w') as datafile:
+			json.dump(data, fp = datafile, indent = 4)
+
+	@commands.command()
+	@commands.has_role("bad coder")
+	async def add(self, ctx, member: discord.Member, project):
+		global data
+		with open('projects.json', 'r') as datafile:
+			try:
+				data = json.load(datafile)
+				data['projects'][project]['members'].append(data['members'][str(member.id)]['name'])
+			except:
+				await ctx.send("Could not find that member.")
+
+bot.add_cog(Project_Leaders(bot))
+
+class General(commands.Cog):
+	def __init__(self, bot):
+		self.bot=bot
+
+	@commands.command()
+	async def list(self, ctx, what, open_only='open'):
+		with open('projects.json', 'r') as datafile:
+			data=json.load(datafile)
+			line=""
+			if (what=="projects"):
+				for project in data["projects"].keys():
+					value = data["projects"][project]
+					status = value['status']
+					if open_only=="open":
+						if status=='open':
+							members = ""
+							for member in value["members"]:
+								members += member+", "
+							members = members[:len(members)-2]
+
+							formatted = str(project)+": "+value["link"]+"\n"+members+"\n\n"
+
+							line+=formatted
+					elif open_only=='all':
 						members = ""
 						for member in value["members"]:
 							members += member+", "
@@ -116,63 +143,38 @@ async def list(ctx, what, open_only='open'):
 						formatted = str(project)+": "+value["link"]+"\n"+members+"\n\n"
 
 						line+=formatted
-				elif open_only=='all':
-					members = ""
-					for member in value["members"]:
-						members += member+", "
-					members = members[:len(members)-2]
 
-					formatted = str(project)+": "+value["link"]+"\n"+members+"\n\n"
-
-					line+=formatted
-
-			if line.strip()=="":
-				line = "no open projects currently"
-			else:
+				if line.strip()=="":
+					line = "no open projects currently"
+				else:
+					line = line[:len(line)-2]
+				await ctx.send(line)
+			elif (what=="members"):
+				for member in data["members"].values():
+					line+=str(member["name"]+", ")
 				line = line[:len(line)-2]
-			await ctx.send(line)
-		elif (what=="members"):
-			for member in data["members"].values():
-				line+=str(member["name"]+", ")
-			line = line[:len(line)-2]
-			await ctx.send(line)
-		else:
-			await ctx.send("Please type in members or projects :)")
+				await ctx.send(line)
+			else:
+				await ctx.send("Please type in members or projects :)")
+
+	@commands.command()
+	async def project(self, ctx, name):
+		pass
+
+	@commands.command()
+	async def updateuser(self, ctx, Name, email):
+		global data
+		names = Name.split('-')
+		name = names[0] + ' ' + names[1]
+		with open('projects.json', 'r') as datafile:
+			data = json.load(datafile)
+			data['members'][str(ctx.author.id)] = {"name": name, "status": "Member", "email": email}
+
+		with open('projects.json', 'w') as datafile:
+			json.dump(data, fp = datafile, indent = 4)
+
+bot.add_cog(General(bot))
 
 
-
-@bot.command()
-async def create(ctx, name, link):
-	# new={
-	# "members" :[],
-	# "link":link,
-	# "status": "open"
-	# }
-	with open('projects.json','r') as datafile:
-		data=json.load(datafile)
-		new={
-		"members" :[data['members'][str(ctx.author.id)]["name"]],
-		"link":link,
-		"status": "open"
-		}
-		data["projects"][name] = new
-	with open('projects.json', 'w') as datafile:
-		json.dump(data, fp = datafile, indent = 4)
-
-@bot.command()
-async def project(ctx, name):
-	pass
-
-@bot.command()
-async def updateuser(ctx, Name, email):
-	global data
-	names = Name.split('-')
-	name = names[0] + ' ' + names[1]
-	with open('projects.json', 'r') as datafile:
-		data = json.load(datafile)
-		data['members'][str(ctx.author.id)] = {"name": name, "status": "Member", "email": email}
-
-	with open('projects.json', 'w') as datafile:
-		json.dump(data, fp = datafile, indent = 4)
 
 bot.run(TOKEN)
